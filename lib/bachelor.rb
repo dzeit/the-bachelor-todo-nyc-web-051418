84ccf9863469e39df632b4c029e28c2d9c71dc1c
David Zeit <dzeit216@gmail.com>
2018-05-03 21:22:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,6 +37,7 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
+  counter = 0 
   data.each do |season, contestant_array|
     contestant_array.each do |contestant|
       if contestant["hometown"] == hometown  
