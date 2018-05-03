23a3d936c3b3935eafb818b9270e2f4e5b23bfbd
David Zeit <dzeit216@gmail.com>
2018-05-03 19:59:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,27 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season] 
+  data[season].each do |array_of_hash|
+    #array_of_hash[:status] to get access to the value of :status key
+    if array_of_hash["status"] == "Winner"
+      return array_of_hash["name"].split[0]
+    end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  # loop over data
+  # key, values 
+  #values loop over 
+  # contestant occupation  
+  data.each do |season, contestant_array| #season = 1 key of many keys / contestant_array = the values of the key (the first iteration of data)
+    contestant_array.each do |contestant|
+      if contestant["occupation"] == occupation 
+        return contestant["name"]
+      end 
+    end 
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
